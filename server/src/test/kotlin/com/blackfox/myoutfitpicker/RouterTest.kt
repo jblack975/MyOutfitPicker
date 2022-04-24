9d95346d362f51d0e0a1442236374529619d50cb
jblack975 <79993915+jblack975@users.noreply.github.com>
2022-04-23 22:29:21 -0500

    Add initial server tests (#7)
    * Applying to make certain .gitignore worked.
    
    * Applying new changes.
    
    * Added the initial tests.

--- server/src/test/kotlin/com/blackfox/myoutfitpicker/RouterTest.kt
+++ server/src/test/kotlin/com/blackfox/myoutfitpicker/RouterTest.kt
@@ -1,16 +1,25 @@
 package com.blackfox.myoutfitpicker
 
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class RouterTest {
     @Test
-    fun testExample() {
-        //assertTrue(runBlocking {  WeatherApi(createHttpClient()).retrieveMonthlyForecastByCity("London,ON,CA")?.list?.size!! > 5})
+    fun testWeatherCurrentCity() = testApplication {
+        val response = client.get("/weather/current/Seattle")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("calling for current weather in Seattle", response.bodyAsText())
     }
     @Test
-    fun testCurrentWeather() {
-        //assertTrue(runBlocking {  WeatherApi(createHttpClient()).retrieveCurrentWeatherByCity("London,Uk")?.weather?.size!! > 0})
+    fun testWeatherCurrent() = testApplication {
+        val response = client.get("/weather/current")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("calling for current weather in this city", response.bodyAsText())
     }
 }
